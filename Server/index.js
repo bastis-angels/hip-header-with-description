8393938975c38d8e2f53c5af1ea95fda950696f1
admin <admin@admins-MacBook-Pro.local>
2019-03-29 18:00:48 -0700

    Implement a succesful get request to a specific campsite.

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const Camps = require('../database-mongodb/Camp.js')
+
+app.get('/:campsiteId', (req, res) => {
+  console.log(req.params)
+    Camps.find({ newId: req.params.campsiteId}, (err, camps)=> {
+        if (err){
+          res.send(err)
+        }else {
+          res.send(camps)
+        }
+      });   
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
