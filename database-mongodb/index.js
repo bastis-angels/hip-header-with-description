91a540ab32224fa422895595a1337f2c5d2f3177
admin <admin@admins-mbp.lan>
2019-03-30 02:21:07 -0700

    correct the syntax of the database index file.

--- database-mongodb/index.js
+++ database-mongodb/index.js
@@ -4,7 +4,7 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log("connected")
+  console.log("connected");
 });
 
 module.exports = database
