d4fe67be914abf4914f2ab0b6e51cd8f50c0d13e
admin <admin@admins-MacBook-Pro.local>
2019-04-03 13:56:18 -0700

    Add jest and enzyme dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,26 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/Client/src');
+
+const DIST_DIR = path.join(__dirname, '/Client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
+  devtool: 'inline-source-map',
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
   },
   module: {
     loaders: [
       {
-        test: /\.jsx?/,
+        test: /\.jsx$/,
         include: SRC_DIR,
         loader: 'babel-loader',
         query: {
-          presets: ['@babel/preset-react', 'airbnb', '@babel/preset-env']
-        }
-      }
-    ]
-  }
+          presets: ['@babel/preset-react', '@babel/preset-env', 'babel-preset-airbnb'],
+        },
+      },
+    ],
+  },
 };
